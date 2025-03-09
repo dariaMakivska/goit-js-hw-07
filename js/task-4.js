a35362cf405ca117b2fd80176345b7a58c09b884
Daria Makivska <peony.bluet@gmail.com>
2025-03-09 02:12:01 +0200

    Додає JS та CSS task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,35 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const elements = event.target.elements;
+  const info = {
+    email: elements.email.value.trim(),
+    password: elements.password.value.trim(),
+  };
+
+  if (info.email === "" || info.password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+  console.log(info);
+  form.reset();
+}
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", function () {
+    if (this.value === "") {
+      this.placeholder = "Type area";
+    }
+  });
+
+  input.addEventListener("blur", function () {
+    if (this.value === "") {
+      this.placeholder = "";
+    }
+  });
+});
