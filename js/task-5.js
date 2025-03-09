e8870582765a73dd0a15250b6145532f37ea80eb
Daria Makivska <peony.bluet@gmail.com>
2025-03-09 02:40:59 +0200

    Додає JS та CSS task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,27 @@
+{
+  /* <div class="widget">
+  <p>
+    Background color: <span class="color">-</span>
+  </p>
+  <button type="button" class="change-color">
+    Change color
+  </button>
+</div>; */
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+button.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  span.textContent = `${randomColor}`;
 }
